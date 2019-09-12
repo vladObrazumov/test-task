3a5211c4d56727740a43a8a74c021c6905f0289c
Vlad Obrazumov <vlad.obrazumov@gmail.com>
2019-09-12 20:30:54 +0300

    Create IntutorService to fetch data

---
+++ src/Service/IntutorService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+
+class IntutorService
+{
+    const SERVICE_HOST = 'http://intutor.no';
+    const VLAD_API_URI = '/vlad_api.json';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => self::SERVICE_HOST,
+            'timeout' => 50,
+        ]);
+    }
+
+    /**
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getData()
+    {
+        $response = $this->client->request("GET", self::VLAD_API_URI);
+        return $response->getBody()->getContents();
+    }
+}
