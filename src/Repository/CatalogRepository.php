170f61886d7350699b8460b5351a2f60f5570fbb
Vlad Obrazumov <vlad.obrazumov@gmail.com>
2019-09-13 08:18:24 +0300

    Extend catelogRepository from nestedTreeRepository

--- src/Repository/CatalogRepository.php
+++ src/Repository/CatalogRepository.php
@@ -2,20 +2,9 @@
 
 namespace App\Repository;
 
-use App\Entity\Catalog;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
-/**
- * @method Catalog|null find($id, $lockMode = null, $lockVersion = null)
- * @method Catalog|null findOneBy(array $criteria, array $orderBy = null)
- * @method Catalog[]    findAll()
- * @method Catalog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class CatalogRepository extends ServiceEntityRepository
+class CatalogRepository extends NestedTreeRepository
 {
-    public function __construct(RegistryInterface $registry)
-    {
-        parent::__construct($registry, Catalog::class);
-    }
+
 }
