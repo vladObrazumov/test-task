6879b0530e307dcb75fe9639ad2e3c18c7d59e17
Vlad Obrazumov <vlad.obrazumov@gmail.com>
2019-09-13 08:53:53 +0300

    Create command to fetch data fro third part service

---
+++ src/Command/FetchDataFromApiCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\CatalogManager;
+use App\Service\IntutorService;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FetchDataFromApiCommand extends Command
+{
+    /**
+     * @var IntutorService
+     */
+    private $intutorService;
+
+    /**
+     * @var CatalogManager
+     */
+    private $catalogManager;
+
+    protected static $defaultName = 'app:fetch-third-part-data';
+
+    public function __construct(IntutorService $intutorService, CatalogManager $catalogManager)
+    {
+        $this->intutorService = $intutorService;
+        $this->catalogManager = $catalogManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Fetch data from third part service');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $data = json_decode($this->intutorService->getCatalogs(), true);
+            $this->catalogManager->createCatalogsFromPureData($data);
+        } catch (GuzzleException $e) {
+            //in fact we must use logger like monolog here
+            $output->writeln("We got error while requesting data, error message: " . $e->getMessage());
+        }
+    }
+}
