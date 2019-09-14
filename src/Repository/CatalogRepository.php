1c5721c50f5be7dac5b4891bf4881aebeda1b4f7
Vlad Obrazumov <vlad.obrazumov@gmail.com>
2019-09-14 20:39:00 +0300

    Add method to get catalogs tree with documents

--- src/Repository/CatalogRepository.php
+++ src/Repository/CatalogRepository.php
@@ -6,5 +6,15 @@ use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
 class CatalogRepository extends NestedTreeRepository
 {
+    public function getCatalogsTreeWithDocuments()
+    {
+        $qb = $this->createQueryBuilder('c');
 
+        $qb->addSelect('d');
+        $qb->leftJoin('c.documents', 'd');
+        $qb->orderBy('c.root');
+        $qb->addOrderBy('c.lft');
+
+        return $this->buildTreeArray($qb->getQuery()->getArrayResult());
+    }
 }
