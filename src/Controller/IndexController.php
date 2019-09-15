1431bef92ca788c59dbea4b7f43120e93c870714
Vlad Obrazumov <vlad.obrazumov@gmail.com>
2019-09-15 15:22:25 +0300

    Pass catalogs tree to the template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Catalog;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -15,16 +14,11 @@ class IndexController extends AbstractController
     public function index()
     {
         $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository(Catalog::class);
-        $htmlTree = $repo->childrenHierarchy(
-            null,
-            false,
-            array(
-                'decorate' => true,
-                'representationField' => 'slug',
-                'html' => true
-            )
-        );
-        return new Response($htmlTree);
+        $catalogRepository = $em->getRepository(Catalog::class);
+        $catalogs = $catalogRepository->getCatalogsTreeWithDocuments();
+
+        return $this->render('index/index.html.twig', [
+            'catalogs' => $catalogs,
+        ]);
     }
 }
